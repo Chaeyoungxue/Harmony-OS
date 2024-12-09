39261cf710bdddab1da8a53a074a57cf288210f9
xrhhhh <xrh0628163@163.com>
2024-12-10 00:16:08 +0800

    周欣然2

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -2,5 +2,5 @@ import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
     system: hapTasks,  /* songcanBuilt-in plugin of Hvigor. It cannot be modified. */
-    plugins:[]         /* 周Custom plugin to extend the functionality of Hvigor. */
+    plugins:[]         /* zhouxin Custom plugin to extend the functionality of Hvigor. */
 }
