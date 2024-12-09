6990a1589e3a459e0f00abb3bb52acd1e9c327e4
Chaeyoungxue <chaechaexue@gmail.com>
2024-12-09 23:21:06 +0800

    示例

--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,6 +1,6 @@
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
-    system: appTasks,  /* Build-plugin of Hvigor. It cannot be modified. 你好刘慧克*/
+    system: appTasks,  /* Build-plugin of Hvigor. It cannot be modified.*/
     plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
 }
