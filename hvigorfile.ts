3cabe9af21b6edac847e1c9ca6c15de7149b98b0
Chaeyoungxue <chaechaexue@gmail.com>
2024-12-09 23:08:09 +0800

    try

--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,6 +1,6 @@
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
-    system: appTasks,  /* nihBuilin plugin of Hvigor. It cannot be modified. */
+    system: appTasks,  /* nihBuiln plugin of Hvigor. It cannot be modified. */
     plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
 }
