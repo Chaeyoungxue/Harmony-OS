7cdddc71f209d143009ab6f125f8b4cb957f5991
Chaeyoungxue <chaechaexue@gmail.com>
2024-12-09 23:19:37 +0800

    上传初始文件

--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,6 +1,6 @@
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
-    system: appTasks,  /* Build-plugin of Hvigor. It cannot be modified. */
+    system: appTasks,  /* Build-plugin of Hvigor. It cannot be modified. 你好刘慧克*/
     plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
 }
